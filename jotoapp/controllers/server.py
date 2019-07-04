8447262a7d0fd1d8ad06a04567e391d9b27d2284
Your Name <you@example.com>
2019-07-04 22:07:06 +0900

    ボタン配置途中

--- jotoapp/controllers/server.py
+++ jotoapp/controllers/server.py
@@ -1,6 +1,10 @@
 import logging
 
+
+from flask import jsonify
+from flask import request
 from flask import render_template
+
 import config
 
 
@@ -15,6 +19,12 @@ def index():
 @app.route('/controller/')
 def controller():
     return render_template('controller.html')
+
+@app.route('/api/command/', methods=['POST'])
+def command():
+    cmd = request.form.get('command')
+    logger.info({'action': 'command', 'cmd':cmd})
+    return jsonify(status='success'), 200
 
 def run():
     app.run(host=config.WEB_ADDRESS, port=config.WEB_PORT, threaded=True)
