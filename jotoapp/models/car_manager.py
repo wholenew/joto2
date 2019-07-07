d9303976d5b4cee2f886ba966f93b3b7c319aea9
Your Name <you@example.com>
2019-07-07 21:09:02 +0900

    ラズパイからArduinoへシリアル通信

--- jotoapp/models/car_manager.py
+++ jotoapp/models/car_manager.py
@@ -3,6 +3,7 @@ import socket
 import sys
 import time
 import pigpio
+import smbus
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
@@ -13,19 +14,27 @@ class CarManager(object):
         self.response = None
         self.pi = pigpio.pi()
         self.servo = servo
-
+        self.bus = smbus.SMBus(1)
+        self.address = 0x04
 
   def send_command(self, command):
       logger.info({'action': 'send_command', 'command': command})
+      code = ''
       if command=='forward':
         pwm = 900
+        code='f'
       elif command=='back':
         pwm = 1300
+        code='b'
       elif command=='right':
         pwm = 1700
+        code='r'
       elif command=='left':
         pwm = 2100
+        code='l'
+      
       self.pi.set_servo_pulsewidth(self.servo, pwm)
+      self.bus.write_byte(self.address, ord(code))
       time.sleep(3)
 
       return self.response
