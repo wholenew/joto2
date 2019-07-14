a31adddfdd03560cf0880782a4ce159c0a04351b
Your Name <you@example.com>
2019-07-14 14:08:32 +0900

    shebang実装

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3
+
 import logging
 import sys
 
 import config
 
 import jotoapp.controllers.server
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.CRITICAL,
                     stream=sys.stdout,
                     #filename=config.LOG_FILE
                     )
 
 
-if __name__ == '__main__':
-    jotoapp.controllers.server.run()
+jotoapp.controllers.server.run()
